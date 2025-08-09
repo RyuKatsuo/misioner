a2439c0851caceedfb3b8885d1602d1522fb3231
AnandaAndreas <anandaandreas@students.amikom.ac.id>
2025-08-09 19:12:28 +0700

    change child card

--- app/Http/Controllers/User/ChildController.php
+++ app/Http/Controllers/User/ChildController.php
@@ -15,6 +15,8 @@ use Inertia\Response;
 use Intervention\Image\Encoders\AutoEncoder;
 use Intervention\Image\Laravel\Facades\Image;
 
+use function Illuminate\Log\log;
+
 class ChildController extends Controller
 {
     public function index(): Response
@@ -83,6 +85,8 @@ class ChildController extends Controller
 
         $qrCodePath = $child->qr_url ? Storage::disk('public')->path($child->qr_url): null;
         $avatarPath = $child->avatar_url ? Storage::disk('public')->path($child->avatar_url): null;
+        
+        
 
         $data = [
             'child' => $child,
